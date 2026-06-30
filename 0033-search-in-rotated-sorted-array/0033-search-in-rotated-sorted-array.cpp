1f6d49f10cc7be8def8717d1543cddcad5504fb0
Anurag Tomar <anuragtomar259@gmail.com>
2026-06-30 22:10:00 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,53 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    // Function to search for target using binary search in rotated sorted array
     int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
 
-        // Set the search space to entire array
-        int low = 0;
-        int high = nums.size() - 1;
-
-        // Continue until the search space becomes invalid
         while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            // Find the middle index
-            int mid = (low + high) / 2;
-
-            // If the target is found at mid, return mid
             if (nums[mid] == target)
                 return mid;
 
-            // Check if the left half is sorted
+            // Left half sorted
             if (nums[low] <= nums[mid]) {
-
-                // If target lies in the sorted left half, search there
                 if (nums[low] <= target && target < nums[mid]) {
                     high = mid - 1;
-                }
-                // Else search in the right half
-                else {
+                } else {
                     low = mid + 1;
                 }
             }
-
-            // Otherwise, right half is sorted
+            // Right half sorted
             else {
-
-                // If target lies in the sorted right half, search there
                 if (nums[mid] < target && target <= nums[high]) {
                     low = mid + 1;
-                }
-                // Else search in the left half
-                else {
+                } else {
                     high = mid - 1;
                 }
             }
         }
 
-        // If not found, return -1
         return -1;
     }
 };
