5317963db73dd0aa6aafd2fbeb002667b6e6cb07
Anurag Tomar <anuragtomar259@gmail.com>
2026-06-30 11:43:56 +0530

    Time: 0 ms (100%), Space: 15.2 MB (49.29%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,16 +1,54 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-
-        vector<int> ans;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                ans.push_back(i);   // Store index
+    int firstoccurence(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int first = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        return ans;
+        return first;
+    }
+    int lastOccurrence(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int last = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return last;
+    }
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> res;
+        int first = firstoccurence(nums, target);
+        if (first == -1)
+            return res;
+
+        int last = lastOccurrence(nums, target);
+
+        for (int i = first; i <= last; i++) {
+            res.push_back(i);
+        }
+        return res;
     }
 };
