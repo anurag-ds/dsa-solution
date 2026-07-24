18ea377f4e2f9b51f0d503c42c24183eb321a6ef
Anurag Tomar <anuragtomar259@gmail.com>
2026-07-24 22:40:15 +0530

    Time: 446 ms (34.76%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
-SELECT
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
-FROM Person p
-LEFT JOIN Address a ON p.personId = a.personId;
+# Write your MySQL query statement below
+select Person.firstName, Person.lastName, Address.city ,Address.state 
+FROM Person
+LEFT JOIN Address
+ON Person.personId = Address.personId;
 
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
