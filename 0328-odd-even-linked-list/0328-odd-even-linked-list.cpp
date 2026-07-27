3b03d86e11e03bd963441e050947617a64ea3a5f
Anurag Singh Tomar <anuragtomar259@gmail.com>
2026-07-27 20:48:28 +0530

    Time: 0 ms (100%), Space: 15.7 MB (29.26%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr)
+            return head;
+        ListNode*odd = head;
+        ListNode*even = odd->next;
+        ListNode*evenhead = odd->next;
+        while(even != nullptr && even->next != nullptr){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenhead;
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
