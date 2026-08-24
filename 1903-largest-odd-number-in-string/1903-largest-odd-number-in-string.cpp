4fa221a1069aa5398a543a9ffff3c4596c4cc76d
Anurag Singh Tomar <anuragtomar259@gmail.com>
2026-08-24 23:07:54 +0530

    Time: 0 ms (100%), Space: 17.8 MB (38.48%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+
+        for(int i = num.size() - 1; i >= 0; i--) {
+
+            if((num[i] - '0') % 2 != 0) {
+                return num.substr(0, i + 1);
+            }
+        }
+
+        return "";
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
