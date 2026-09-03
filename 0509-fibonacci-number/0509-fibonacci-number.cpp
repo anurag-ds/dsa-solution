dff3d662e657ae435327774908f2ff41f842dd4c
Anurag Singh Tomar <anuragtomar259@gmail.com>
2026-09-03 23:19:28 +0530

    Time: 14 ms (22.17%), Space: 7.6 MB (88.59%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,8 +4,10 @@ public:
         if(n==0|| n==1){
             return n;
         }
-        
-        return fib(n-1)+fib(n-2);
+
+        int last = fib(n-1);
+        int slast = fib(n-2);
+        return last + slast;
     }
 };
 
