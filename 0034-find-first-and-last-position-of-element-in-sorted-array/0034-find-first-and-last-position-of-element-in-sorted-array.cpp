723f4bdef47f9af1bd5a533bc9c5e4a445048ec1
Anurag Singh Tomar <anuragtomar259@gmail.com>
2026-09-05 23:26:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.61%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -36,3 +36,7 @@ public:
         return {firstoccurence(nums, target), lastoccurence(nums, target)};
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
