816cee83ca1d37db952e98a7dc59614a24bcd2da
Anurag Singh Tomar <anuragtomar259@gmail.com>
2026-09-26 21:48:51 +0530

    Time: 0 ms (100%), Space: 39.2 MB (71.57%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -3,9 +3,7 @@ public:
     bool checkValid(vector<vector<int>>& matrix) {
 
         int n = matrix.size();
-
         for (int i = 0; i < n; i++) {
-
             vector<bool> seen(n + 1, false);
             for (int j = 0; j < n; j++) {
                 int num = matrix[i][j];
