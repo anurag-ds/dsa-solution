213035cf9e82dfbc817465dd10852f902dfc77fc
Anurag Singh Tomar <anuragtomar259@gmail.com>
2026-09-26 21:48:34 +0530

    Time: 2 ms (91.64%), Space: 39 MB (83.17%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++) {
+
+            vector<bool> seen(n + 1, false);
+            for (int j = 0; j < n; j++) {
+                int num = matrix[i][j];
+                if (seen[num]) {
+                    return false;
+                }
+                seen[num] = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+
+            vector<bool> seen(n + 1, false);
+            for (int i = 0; i < n; i++) {
+                int num = matrix[i][j];
+                if (seen[num]) {
+                    return false;
+                }
+                seen[num] = true;
+            }
+        }
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
